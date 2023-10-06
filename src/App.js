d3825dfd16aa1c7bc221a706021ca99ebe8e1f1e
maretabrownargus <maretabrown@gmail.com>
2023-10-07 08:30:05 +1000

    added github url to footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by Mareta Brown and is{" "}
+        <a
+          href=" https://github.com/maretabrownargus/react.weather.app.git"
+          target="blank"
+        >
+          open sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
