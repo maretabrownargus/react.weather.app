84cf09c91c0a7e21822143a4ceb7de0cdb417787
maretabrownargus <maretabrown@gmail.com>
2023-10-15 09:43:36 +1000

    added new component WeatherForecast  js & css files , added new weatherforecast day info.

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="1elele"
+      color="Orange"
       size={props.size}
       animate={true}
     />
