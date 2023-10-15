2d18756d159d72679a667cc58777cb4560da58af
maretabrownargus <maretabrown@gmail.com>
2023-10-15 19:08:56 +1000

    fixing some css issues

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -14,18 +14,21 @@ export default function WeatherInfo(props) {
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
       <div className="row">
-        <div className="col-6">
-          <WeatherIcon code={props.data.icon} size={60} />
+        <div className="col-md-6">
+          <div className="float-left">
+            <WeatherIcon code={props.data.icon} size={50} />
+          </div>
+          <div className="float-left">
+            <WeatherTemperature celsius={props.data.temperature} />
+          </div>
         </div>
-        <div className="col-9">
-          <WeatherTemperature celsius={props.data.temperature} />
+
+        <div className="col-md-6">
+          <ul>
+            <li>Humidity: {props.data.humidity} </li>
+            <li>Wind:{props.data.wind} km/h</li>
+          </ul>
         </div>
-      </div>
-      <div className="col-6">
-        <ul>
-          <li>Humidity: {props.data.humidity} </li>
-          <li>Wind:{props.data.wind} km/h</li>
-        </ul>
       </div>
     </div>
   );
